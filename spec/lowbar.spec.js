7e96cf4d225ff431b57f336329e0b5ff8fded0c8
Kavita <kavi.b0208@gmail.com@github.com>
2017-07-04 20:33:24 +0100

    initial commit

---
+++ spec/lowbar.spec.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const expect = require('chai').expect;
+
+const _ = require(path.join(__dirname, '..', './lowbar.js'));
+
+describe('_', function () {
+  'use strict';
+
+  it('is an object', function () {
+    expect(_).to.be.an('object');
+  });
+});
